28f1d01fe4f72ef46eb7c97d62a6cc7fb06b7cbf
Abdul Majeed <dev.abdul.majeed@gmail.com>
2026-09-13 20:19:57 +0100

    Fixed basename to deploy

--- src/route/router.jsx
+++ src/route/router.jsx
@@ -7,31 +7,34 @@ import SingleProject from "../pages/single-project";
 import Projects from "../pages/projects";
 
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <RootLayout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />
-            },
-            {
-                path: "/contact",
-                element: <Contact />
-            },
-            {
-                path: "/education_experience",
-                element: <Edu_Exp />
-            },
-            {
-                path: "/single-project",
-                element: <SingleProject />
-            },
-            {
-                path: "/projects",
-                element: <Projects />
-            },
-        ]
-    }
-])
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <RootLayout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />
+                },
+                {
+                    path: "/contact",
+                    element: <Contact />
+                },
+                {
+                    path: "/education_experience",
+                    element: <Edu_Exp />
+                },
+                {
+                    path: "/single-project",
+                    element: <SingleProject />
+                },
+                {
+                    path: "/projects",
+                    element: <Projects />
+                },
+            ]
+        }
+    ],
+    { basename: "/portfolio" }
+)
